f2ceadab172efa99b94ff4c4e8832ed96ea01811
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-14 13:00:17 -0500

    Arreglado

--- src/TransferenciaBancaria.java
+++ src/TransferenciaBancaria.java
@@ -7,7 +7,7 @@ public class TransferenciaBancaria extends MetodoPago{
 
     @Override
     public void realizarPago(double monto) {
-        double cobro=monto+(monto*0.05);
+        double cobro=monto+(monto*5);
         if(saldoDisponible>=cobro){
             saldoDisponible-=cobro;
             System.out.println("Su transaccion es de : $"+cobro);
